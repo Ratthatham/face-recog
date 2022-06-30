9884e809d26273c78f92d2ffa4bde1ed054f62ca
Ratthatham siridol <ratthathamsiridol@Ratthathams-Macbook-Air.local>
2022-06-30 22:47:32 +0700

    Add Rank.js

--- src/component/Rank/Rank.js
+++ src/component/Rank/Rank.js
@@ -3,6 +3,9 @@ import React from "react";
 function Rank() {
     return(
         <div className = 'Rank'>
+            <div className = 'white f3'>
+                {'Ratthatham, Your Rank is...'}
+            </div>
             
             
         </div>
